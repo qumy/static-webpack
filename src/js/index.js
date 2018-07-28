2383eb48a985c6d6727d4b6c5e03acfdf0ff073a
qumy <qumy@yandex.ru>
2018-07-28 18:47:11 +0500

    web static page by webpack

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import jQuery from 'jquery';
+import popper from 'popper.js';
+import bootstrap from 'bootstrap';
+
+jQuery(function() {
+    jQuery('body').fadeTo( "slow" , 0.8, function() {
+        // Animation complete.
+      });
+});
